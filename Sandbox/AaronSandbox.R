b9dbbcefed7f184d8842246b36a967f3f91ec5e3
aswoboda <aaron.swoboda@gmail.com>
2012-08-16 12:05:30 -0500

    initial figures for analyzing our results

--- Sandbox/AaronSandbox.R
+++ Sandbox/AaronSandbox.R
@@ -1,23 +1,23 @@
 
 
 
-begin = Sys.time()
 
-end = Sys.time()
+# playing around with making plots to display optimal bandwidths under GCV vs SCV
 
-times = end - begin
-
-class(times)
-
-paste("time dif", print(times))
-
-Replications = 500
-sample.size = c(50, 100, 200, 500)
-error.sd = c(.3, 1)
-B1.spatial.var = c(.5, 1)
-B2.spatial.var = 1
- 
-# expand the parameter vectors and create a container for our simulation output
-sim.parameters = expand.grid(sample.size, error.sd, B1.spatial.var, B2.spatial.var)
-names(sim.parameters) = c("sample.size", "error.sd", "B1.spatial.var", "B2.spatial.var")
-sim.parameters
+pdf("SpecificationSims/Figures/simOutputFigures.pdf")
+par(mfrow = c(2, 2))
+for (k in 1:4){
+  for (i in 1:2) {
+    for (j in 1:2) {
+      plot(simOutput[k, i, j, 1, , "GCV"], simOutput[k, i, j, 1, , "SCV"],
+           xlim = c(0, 500), xlab = "GCV",
+           ylim = c(0, 500), ylab = "SCV",
+           main = paste("mSCV = ", round(mean(simOutput[k, i, j, 1, , "SCV"]), 0),
+                        "mGCV = ", round(mean(simOutput[k, i, j, 1, , "GCV"]), 0)),
+           pch = 16, cex = .65,
+           col = rgb(red = 0, green = 0, blue = 1, alpha = .05)
+      )
+    }
+  }
+}
+dev.off()
