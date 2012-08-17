6d97cdb269f058a8bdbc02dcfe021c7dd7100a41
aswoboda <aaron.swoboda@gmail.com>
2012-08-17 17:40:01 -0500

    updated uberScript to run over the weekend

--- Sandbox/AaronSandbox.R
+++ Sandbox/AaronSandbox.R
@@ -17,71 +17,52 @@ meta.sim.num = dim(sim.parameters)[1]
 
 DGPparameters = sim.parameters[1, ]
 
+
+
 simulation(1, DGPparameters)
-    
-
-Data = DataGen(DGPparameters$sample.size, 
-               DGPparameters$error.sd, 
-               DGPparameters$B1.spatial.var, 
-               DGPparameters$B2.spatial.var)
-output = lapply(1:dim(Data)[1], LWR, Data.Frame = Data)
-new.output = Reorganizer(output)
-simMetrics = LWRMetrics(new.output, Data)
-optimal.bandwidths = bandwidth.Selector(simMetrics)
-Rsquareds = RsquaredComparer(Data)
-list(c(optimal.bandwidths, Rsquareds))
-
-LWRMetrics = function(LWRinput, Data) {
-  bandwidths = LWRinput$bandwidths
-  if(sd(Data$trueB0) == 0) {
-    beta0.cor.results = rep(NA, length(bandwidths))
-  } else beta0.cor.results = cor(LWRinput$beta0hats, Data$trueB0) # B0
-  
-  if(sd(Data$trueB1) == 0) {
-    beta1.cor.results = rep(NA, length(bandwidths))
-  } else beta1.cor.results = cor(LWRinput$beta1hats, Data$trueB1) #B1
-  
-  if(sd(Data$trueB2) == 0) {
-    beta2.cor.results = rep(NA, length(bandwidths))
-  } else beta2.cor.results = cor(LWRinput$beta2hats, Data$trueB2)#B2
-  
-  dep.var.cor.results = cor(LWRinput$yhats, Data$dep.var) #dependent variable
-  
-  
-  # Residuals: For beta0/1/2
-  # beta.Residual.calc is a function defined in SimFunctions and can be sourced
-  beta0.residuals = beta.Residual.Calc(LWRinput[["beta0hats"]], Data$trueB0)
-  beta1.residuals = beta.Residual.Calc(LWRinput[["beta1hats"]], Data$trueB1)
-  beta2.residuals = beta.Residual.Calc(LWRinput[["beta2hats"]], Data$trueB2)
-  
-  # Now calculate for each beta the % of t-tests with values > critical t, for each k.
-  beta0.ttest.percent = beta.ttest(LWRinput[["beta0hats"]], LWRinput[["ses0"]], Data$trueB0, LWRinput$bandwidths)
-  beta1.ttest.percent = beta.ttest(LWRinput[["beta1hats"]], LWRinput[["ses1"]], Data$trueB1, LWRinput$bandwidths)
-  beta2.ttest.percent = beta.ttest(LWRinput[["beta2hats"]], LWRinput[["ses2"]], Data$trueB2, LWRinput$bandwidths)
-  
-  # Generalized Cross-validation score
-  
-  gcv.values = GCV(LWRinput[["leverages"]], LWRinput[["yhats"]], Data$dep.var)
-  ##Define GCV score
-  
-  # Standardized CV a la Paez 2007
-  stan.gcv.values = standardized.CV(Data$dep.var, LWRinput[["yhats.without"]]) 
-  
-  data.frame(beta0.cor.results = beta0.cor.results, beta1.cor.results = beta1.cor.results, beta2.cor.results = beta2.cor.results, 
-       dep.var.cor.results = dep.var.cor.results, beta0.residuals = beta0.residuals, 
-       beta1.residuals = beta1.residuals, beta2.residuals = beta2.residuals, 
-       beta0.ttest.percent = beta0.ttest.percent, beta1.ttest.percent = beta1.ttest.percent,
-       beta2.ttest.percent = beta2.ttest.percent, gcv.values = gcv.values, 
-       stan.gcv.values = stan.gcv.values, bandwidths = bandwidths)
-} 
 
 
 
+simRepOut = simulationReplicator(N = 3, DGPparameters)
+
+
+
+simOut = simRepReorganizer(simRepOut)
+
+new.array = array(NA, c(2, 3, 12, 13),
+                  dimnames = list(temp = 1:2,
+                                  replication = dimnames(simOut[[2]])[[1]],
+                                  optimized = dimnames(simOut[[2]])[[2]], 
+                                  metric.values = dimnames(simOut[[2]])[[3]]) )
+dimnames(new.array)
+
+new.array[1, , , ] = simOut[[2]]
 ### Problem - right now we are just grabbing the bandwidths that optimize different metrics, 
 ### we also want to grab the metric values at each of these "optimal" bandwidths to see how they compare
 
 # instead of outputting a vector of bandwidths, will we want a matrix/array?
 
-temp = data.frame(bandwidths = optimal.bandwidths, optimizing = names(optimal.bandwidths))
-merge(temp, simMetrics, sort = F)
+
+[1] "For loop 1 of 4"
+Time difference of 0.08 mins
+[1] "For loop 2 of 4"
+Time difference of 0.3 mins
+[1] "For loop 3 of 4"
+Time difference of 1.37 mins
+[1] "For loop 4 of 4"
+Time difference of 11.62 mins
+> 
+  > proc.time()
+user   system  elapsed 
+1329.587    0.852  802.615 
+
+
+
+
+
+
+
+
+
+
 
--- SpecificationSims/uberScript.R
+++ SpecificationSims/uberScript.R
@@ -3,11 +3,11 @@
 source("SpecificationSims/SimFunctions.R")
 
 # set our simulation parameters
-Replications = 50
-sample.size = c(50, 100, 200)
+Replications = 100
+sample.size = c(50, 100, 200, 500)
 error.sd = c(1, 3, 5)
 B1.spatial.var = c(0, .25, .75)
-B2.spatial.var = c(0, .25)
+B2.spatial.var = c(0, .25, .75)
 
 # expand the parameter vectors and create a container for our simulation output
 sim.parameters = expand.grid(error.sd, B1.spatial.var, B2.spatial.var, sample.size)
@@ -15,21 +15,36 @@ names(sim.parameters) = c("error.sd", "B1.spatial.var", "B2.spatial.var", "sampl
 
 meta.sim.num = dim(sim.parameters)[1]
 
-simOutput = array(NA, c(length(sample.size),
+R2Output = array(NA, c(length(sample.size),
+                           length(error.sd),
+                           length(B1.spatial.var),
+                           length(B2.spatial.var),
+                           Replications,
+                           2),
+                     dimnames = list(ss = sample.size,
+                                     error.sd = error.sd,
+                                     B1sv = B1.spatial.var,
+                                     B2sv = B2.spatial.var,
+                                     simNum = 1:Replications,
+                                     R2 = c("OLS", "LWR")       ))
+
+MetricOutput = array(NA, c(length(sample.size),
                         length(error.sd),
                         length(B1.spatial.var),
                         length(B2.spatial.var),
                         Replications,
-                        14),
+                        12, 13),
                   dimnames = list(ss = sample.size,
                                   error.sd = error.sd,
                                   B1sv = B1.spatial.var,
                                   B2sv = B2.spatial.var,
                                   simNum = 1:Replications,
-                                  metric = c("GCV", "SCV", "SSRB0", "SSRB1", "SSRB2", 
-                                             "ttest%B0", "ttest%B1", "ttest%B2", 
-                                             "corB0", "corB1", "corB2", "corDepVar",
-                                             "R2OLS", "R2LWR")
+                                  optimized = c("corB0", "corB1", "corB2", "CV", "GCV",
+                                                "RMSE.B0", "RMSE.B1", "RMSE.B2", "SCV",
+                                                "ttest%B0", "ttest%B1", "ttest%B2"),
+                                  metric = c("bandwidths", "B0.cor", "B1.cor", "B2.cor",
+                                             "B0.RMSE", "B1.RMSE", "B2.RMSE",
+                                             "B0.t.perc", "B1.t.perc", "B2.t.perc", "GCV", "SCV", "CV")
                   )
 )
 
@@ -37,13 +52,21 @@ simOutput = array(NA, c(length(sample.size),
 
 for( i in 1:meta.sim.num) { 
   start = Sys.time()
-  simOutput[as.character(sim.parameters[i, "sample.size"]),
+  simRepOut = simulationReplicator(Replications, sim.parameters[i, ], MC = TRUE)
+  simOut = simRepReorganizer(simRepOut)
+  
+  R2Output[as.character(sim.parameters[i, "sample.size"]),
+               as.character(sim.parameters[i, "error.sd"]),
+               as.character(sim.parameters[i, "B1.spatial.var"]),
+               as.character(sim.parameters[i, "B2.spatial.var"]), , ] = simOut[[1]]
+  
+  MetricOutput[as.character(sim.parameters[i, "sample.size"]),
             as.character(sim.parameters[i, "error.sd"]),
             as.character(sim.parameters[i, "B1.spatial.var"]),
-            as.character(sim.parameters[i, "B2.spatial.var"]), , ] = as.matrix(simulationReplicator(Replications, sim.parameters[i, ], MC = TRUE))
+            as.character(sim.parameters[i, "B2.spatial.var"]), , , ] = simOut[[2]]
   end = Sys.time()
 
   print(paste("For loop", i,"of", meta.sim.num))
   print(round(difftime(end, start, units = "m"), 2))
-  save(simOutput, file = "SpecificationSims/uberScriptOutput.RData")
+  save(R2Output, MetricOutput, file = "SpecificationSims/uberScriptOutput.RData")
 }
