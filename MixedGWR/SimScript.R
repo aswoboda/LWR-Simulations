668252d2bb8b6ffc7f43c2145eb814b87dbe9200
aswoboda <aaron.swoboda@gmail.com>
2013-09-09 22:23:22 -0500

    added reps = 100

--- MixedGWR/SimScript.R
+++ MixedGWR/SimScript.R
@@ -6,7 +6,7 @@ B0.SpVar <- c(0) # , 2, 4
 B1.SpVar <- c(0) #, 2, 4
 B2.SpVar <- c(0) # , 2, 4
 errors <- c(0.5) #, 1, 2
-numRepeats <- 50
+numRepeats <- 100
 
 testParams <- expand.grid(numRepeats = numRepeats, B0SpatialVar = B0.SpVar, B1SpatialVar = B1.SpVar, B2SpatialVar = B2.SpVar, errorSD = errors, sampleSizes = sampleSizes)
 
