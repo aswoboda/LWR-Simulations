8189c9a193647653b93c92db20fb07085ed00070
aswoboda <aaron.swoboda@gmail.com>
2013-10-08 15:10:15 -0500

    ran full script

--- MixedGWR/SimScript.R
+++ MixedGWR/SimScript.R
@@ -2,11 +2,11 @@
 source("MixedGWR/helperFunctions.R")
 
 sampleSizes <- c(100, 300, 600, 1000) 
-B0.SpVar <- c(0) # , 2, 4
-B1.SpVar <- c(0) #, 2, 4
-B2.SpVar <- c(0) # , 2, 4
-errors <- c(0.5) #, 1, 2
-numRepeats <- 100
+B0.SpVar <- c(0, 1, 3) # 0, 1, 3
+B1.SpVar <- c(0, 1, 3) # 0, 1, 3
+B2.SpVar <- c(0, 1, 3) # 0, 1, 3
+errors <- c(0.5, 1, 2) # 0.5, 1, 2
+numRepeats <- 200
 
 testParams <- expand.grid(numRepeats = numRepeats, B0SpatialVar = B0.SpVar, B1SpatialVar = B1.SpVar, B2SpatialVar = B2.SpVar, errorSD = errors, sampleSizes = sampleSizes)
 
