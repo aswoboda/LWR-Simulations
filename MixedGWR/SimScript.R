ad7c22518584169ddcb23d04339ed7fd92770591
aswoboda <aaron.swoboda@gmail.com>
2014-02-02 16:00:37 -0600

    ran simulation with models 1 and 8 only, created new folders for output

--- MixedGWR/SimScript.R
+++ MixedGWR/SimScript.R
@@ -2,13 +2,16 @@
 source("MixedGWR/helperFunctionsNewSCV.R")
 set.seed(123452)
 sampleSizes <- c(50, 100, 200, 400, 800) 
-B0.SpVar <- c(0) # , 2, 4
-B1.SpVar <- c(0) #, 2, 4
-B2.SpVar <- c(0) # , 2, 4
+B0.SpVar <- c(0, 2, 4) # 
+B1.SpVar <- c(0, 2, 4) #
+B2.SpVar <- c(0 , 2, 4) #
 errors <- c(0.25, .5, 1, 2, 3) #, 1, 2
 numRepeats <- 100
 
 testParams <- expand.grid(numRepeats = numRepeats, B0SpatialVar = B0.SpVar, B1SpatialVar = B1.SpVar, B2SpatialVar = B2.SpVar, errorSD = errors, sampleSizes = sampleSizes)
+testParams = testParams[which(testParams$B0SpatialVar == testParams$B1SpatialVar), ]
+testParams = testParams[which(testParams$B0SpatialVar == testParams$B2SpatialVar), ]
+
 
 x <- mcMultParams(testParams, MC = T)
 # saves output as "mcOutputFinal.rds" and ultimately "mcOutputFinal.rds"
