0ee8d187f5629f253008d75e645b0bc0d63f603e
aswoboda <aaron.swoboda@gmail.com>
2014-02-23 09:44:19 -0600

    ran a big (675) simulation

--- MixedGWR/SimScript.R
+++ MixedGWR/SimScript.R
@@ -1,11 +1,11 @@
 
 source("MixedGWR/helperFunctionsNewSCV.R")
 set.seed(123452)
-sampleSizes <- c(50) #, 100, 200, 400, 800
-B0.SpVar <- c(0, 2) # , 4
-B1.SpVar <- c(0, 2) #, 4
-B2.SpVar <- c(0, 2) # , 4
-errors <- c(0.25, 3) #, .5, 1, 2
+sampleSizes <- c(50, 100, 200, 400, 800) #
+B0.SpVar <- c(0, 2, 4) # 
+B1.SpVar <- c(0, 2, 4) #
+B2.SpVar <- c(0, 2, 4) # 
+errors <- c(0.25, .5, 1, 2, 3) #
 numRepeats <- 100
 
 testParams <- expand.grid(numRepeats = numRepeats, B0SpatialVar = B0.SpVar, B1SpatialVar = B1.SpVar, B2SpatialVar = B2.SpVar, errorSD = errors, sampleSizes = sampleSizes)
